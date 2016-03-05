2c5ec4c4cce12d7bcc27b88d06a5617a712b702f
Daniela Morais <dani.m@hotmail.co.uk>
2016-03-05 18:00:45 -0300

    Regex p obter bairro

--- src/br/com/smsc/Service/CSVReaderClass.java
+++ src/br/com/smsc/Service/CSVReaderClass.java
@@ -2,14 +2,18 @@ package br.com.smsc.Service;
 
 import au.com.bytecode.opencsv.CSVReader;
 import br.com.smsc.Entity.Doenca;
+import sun.misc.Regexp;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by danielamorais on 3/5/16.
  */
+//FIXME: Refactor
 public class CSVReaderClass {
 
     public void getCSVContent() {
@@ -29,6 +33,13 @@ public class CSVReaderClass {
                             if (fields[j + 1].contains("Total")) break;
                             faixaEtaria.add(j, fields[j + 1]);
                         }
+                    }
+                    if (content.contains("AREA")) {
+                        String[] areaDados = content.split("\"");
+                        String[] bairro = areaDados[0].split(".*AREA ABRANG CS\\s");
+                        //TODO: Regex
+                        System.out.println(bairro[1]);
+
                     }
                 } catch (ArrayIndexOutOfBoundsException e) {
                     i = 0;
